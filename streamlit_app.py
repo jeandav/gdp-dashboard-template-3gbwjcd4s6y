d778403442c1db739dbabfe4d8abf4a9eba7105a
jean <jeandavid.pro@gmail.com>
2024-07-15 17:26:40 +0200

    Import données + première version

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
+    page_title='Titre de la page',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
@@ -22,40 +22,17 @@ def get_gdp_data():
     """
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    DATA_FILENAME = Path(__file__).parent/'data/traitement_drees/livia_lieux_vie_sc1.csv'
+    df = pd.read_csv(DATA_FILENAME, encoding='latin-1', sep=';')
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    df = df[df['genre'] == 'HOMMES']
+    df = df[df['tranche_age'] == '75 ans et plus']
+    df = df[df['hyp_evol_dependance'] == 'stable']
+    df = df[df['hyp_evol_demo'] == 'central']
+    df = df[['dep', 'annee', 'nb_proj_seniors']]
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
+    return df
 
 gdp_df = get_gdp_data()
 
@@ -64,35 +41,34 @@ gdp_df = get_gdp_data()
 
 # Set the title that appears at the top of the page.
 '''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
+# Projection et besoins des populations âgées dépendantes entre 2015 et 2050
 '''
 
 # Add some spacing
 ''
 ''
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+min_value = gdp_df['annee'].min()
+max_value = gdp_df['annee'].max()
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+from_year = min_value
+to_year = max_value
 
-countries = gdp_df['Country Code'].unique()
+# from_year, to_year = st.slider(
+#     'Which years are you interested in?',
+#     min_value=min_value,
+#     max_value=max_value,
+#     value=[min_value, max_value])
+
+countries = gdp_df['dep'].unique()
 
 if not len(countries):
     st.warning("Select at least one country")
 
 selected_countries = st.multiselect(
-    'Which countries would you like to view?',
+    'Sélectionnez un département à afficher',
     countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+    ['1', '2'])
 
 ''
 ''
@@ -100,52 +76,57 @@ selected_countries = st.multiselect(
 
 # Filter the data
 filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
+    (gdp_df['dep'].isin(selected_countries))
+    & (gdp_df['annee'] <= to_year)
+    & (from_year <= gdp_df['annee'])
 ]
 
-st.header('GDP over time', divider='gray')
+st.header('Nombre de seniors projetés', divider='gray')
 
 ''
 
 st.line_chart(
     filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
+    x='annee',
+    x_label='Année',
+    y='nb_proj_seniors',
+    y_label='Nombre de seniors projetés',
+    color='dep',
 )
 
-''
-''
 
+'''
+Champ : France métropolitaine et DOM (sauf Mayotte)
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+Source : modèle LIVIA (DREES) et modèle EP24 (INSEE-DREES)
+'''
 
-st.header(f'GDP in {to_year}', divider='gray')
+# first_year = gdp_df[gdp_df['annee'] == from_year]
+# last_year = gdp_df[gdp_df['annee'] == to_year]
 
-''
+# st.header(f'GDP in {to_year}', divider='gray')
 
-cols = st.columns(4)
+# ''
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+# cols = st.columns(4)
 
-    with col:
-        first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
+# for i, country in enumerate(selected_countries):
+#     col = cols[i % len(cols)]
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+#     with col:
+#         first_gdp = first_year[gdp_df['dep'] == country]['nb_proj_seniors'].iat[0] / 1000000000
+#         last_gdp = last_year[gdp_df['dep'] == country]['nb_proj_seniors'].iat[0] / 1000000000
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+#         if math.isnan(first_gdp):
+#             growth = 'n/a'
+#             delta_color = 'off'
+#         else:
+#             growth = f'{last_gdp / first_gdp:,.2f}x'
+#             delta_color = 'normal'
+
+#         st.metric(
+#             label=f'{country} GDP',
+#             value=f'{last_gdp:,.0f}B',
+#             delta=growth,
+#             delta_color=delta_color
+#         )
