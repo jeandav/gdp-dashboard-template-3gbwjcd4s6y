fbec84d5136f8f11effe94d86afca53856c3125f
jean <jeandavid.pro@gmail.com>
2024-07-16 16:38:49 +0200

    Filtres ajoutés

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,14 @@ import pandas as pd
 import math
 from pathlib import Path
 
+import plotly.express as px
+
+
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='Titre de la page',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    layout="centered"
 )
 
 # -----------------------------------------------------------------------------
@@ -25,11 +29,13 @@ def get_gdp_data():
     DATA_FILENAME = Path(__file__).parent/'data/traitement_drees/livia_lieux_vie_sc1.csv'
     df = pd.read_csv(DATA_FILENAME, encoding='latin-1', sep=';')
 
-    df = df[df['genre'] == 'HOMMES']
-    df = df[df['tranche_age'] == '75 ans et plus']
-    df = df[df['hyp_evol_dependance'] == 'stable']
+    # df = df[df['genre'] == 'HOMMES']
+    # df = df[df['tranche_age'] == '75 ans et plus']
+    df = df[df['hyp_evol_dependance'] == 'intermediaire']
     df = df[df['hyp_evol_demo'] == 'central']
-    df = df[['dep', 'annee', 'nb_proj_seniors']]
+    df = df[['ca', 'tranche_age', 'genre', 'annee', 'nb_proj_seniors']]
+    df = df.groupby(['ca', 'tranche_age', 'genre', 'annee'])['nb_proj_seniors'].sum()
+    df = df.reset_index()
 
 
     return df
@@ -40,6 +46,9 @@ gdp_df = get_gdp_data()
 # Draw the actual page
 
 # Set the title that appears at the top of the page.
+
+st.image('https://upload.wikimedia.org/wikipedia/commons/0/06/Minist%C3%A8re_de_la_Justice.svg', width=100)
+
 '''
 # Projection et besoins des populations âgées dépendantes entre 2015 et 2050
 '''
@@ -60,73 +69,84 @@ to_year = max_value
 #     max_value=max_value,
 #     value=[min_value, max_value])
 
-countries = gdp_df['dep'].unique()
+countries = gdp_df['ca'].unique()
+liste_ca = gdp_df['ca'].unique()
 
 if not len(countries):
     st.warning("Select at least one country")
 
-selected_countries = st.multiselect(
-    'Sélectionnez un département à afficher',
-    countries,
-    ['1', '2'])
+# selected_countries = st.multiselect(
+#     'Sélectionnez un département à afficher',
+#     countries,
+#     ['Gers', 'Jura', 'La Réunion'])
 
+selected_genre = st.selectbox(
+    "Genre :",
+    ("FEMMES", "HOMMES"))
+
+selected_trancheage = st.selectbox(
+    "Tranche d\'age :",
+    ("75 ans et plus", "60-74 ans"))
+
+
+cluster_options = {
+    "Cluster 1" : ['Versailles', 'Paris'],
+    "Cluster 2" : ['Angers', 'Dijon', 'Caen', 'Poitiers', 'Riom', 'Bourges', 'Limoges', 'Agen'],
+    "Cluster 3" : ['Douai', 'Amiens', 'Chambéry', 'Rouen', 'Grenoble', 'Colmar', 'Lyon', 'Reims', 'Metz', 'Toulouse'],
+    "Cluster 4" : ['Rennes', 'Orléans', 'Nancy', 'Besançon', 'Nîmes', 'Aix-en-Provence', 'Montpellier', 'Bordeaux', 'Pau'],
+}
+
+chosen_cluster = st.radio(
+    "Cluster",
+    cluster_options.keys(),
+    horizontal=True
+)
+
+
+selected_ca = st.multiselect(
+    'Cour d\'appel',
+    liste_ca,
+    cluster_options[chosen_cluster])
 ''
 ''
 ''
 
 # Filter the data
 filtered_gdp_df = gdp_df[
-    (gdp_df['dep'].isin(selected_countries))
+    # (gdp_df['dep2'].isin(selected_countries))
+    (gdp_df['ca'].isin(selected_ca))
+    & (gdp_df['genre'] == selected_genre)
+    & (gdp_df['tranche_age'] == selected_trancheage)
     & (gdp_df['annee'] <= to_year)
     & (from_year <= gdp_df['annee'])
 ]
 
-st.header('Nombre de seniors projetés', divider='gray')
+# st.header('Nombre de seniors projetés', divider='gray')
 
 ''
 
-st.line_chart(
-    filtered_gdp_df,
-    x='annee',
-    x_label='Année',
-    y='nb_proj_seniors',
-    y_label='Nombre de seniors projetés',
-    color='dep',
-)
+# st.line_chart(
+#     filtered_gdp_df,
+#     x='annee',
+#     x_label='Année',
+#     y='nb_proj_seniors',
+#     y_label='Nombre de seniors projetés',
+#     color='dep2',
+# )
 
 
-'''
-Champ : France métropolitaine et DOM (sauf Mayotte)
-
-Source : modèle LIVIA (DREES) et modèle EP24 (INSEE-DREES)
-'''
-
-# first_year = gdp_df[gdp_df['annee'] == from_year]
-# last_year = gdp_df[gdp_df['annee'] == to_year]
-
-# st.header(f'GDP in {to_year}', divider='gray')
-
-# ''
-
-# cols = st.columns(4)
-
-# for i, country in enumerate(selected_countries):
-#     col = cols[i % len(cols)]
-
-#     with col:
-#         first_gdp = first_year[gdp_df['dep'] == country]['nb_proj_seniors'].iat[0] / 1000000000
-#         last_gdp = last_year[gdp_df['dep'] == country]['nb_proj_seniors'].iat[0] / 1000000000
-
-#         if math.isnan(first_gdp):
-#             growth = 'n/a'
-#             delta_color = 'off'
-#         else:
-#             growth = f'{last_gdp / first_gdp:,.2f}x'
-#             delta_color = 'normal'
-
-#         st.metric(
-#             label=f'{country} GDP',
-#             value=f'{last_gdp:,.0f}B',
-#             delta=growth,
-#             delta_color=delta_color
-#         )
+fig = px.line(filtered_gdp_df, x="annee", y="nb_proj_seniors", color='ca', markers=True)
+fig.update_layout(
+    yaxis_title='Nombre de seniors projetés',
+    xaxis_title='Année',
+    legend_title=None,
+    legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="right",
+        x=1
+        )
+    )
+fig.add_vline(x=2024, line_width=1, line_color="lightgrey")
+st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
